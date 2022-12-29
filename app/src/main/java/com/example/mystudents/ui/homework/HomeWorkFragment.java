9347a465c7d8dcf9ef2462b63ae663e44a50e3ff
Shujay10 <shujayke@gmail.com>
2022-12-30 00:16:42 +0530

    HW Dialog Ui correction over

--- app/src/main/java/com/example/mystudents/ui/homework/HomeWorkFragment.java
+++ app/src/main/java/com/example/mystudents/ui/homework/HomeWorkFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -40,7 +41,7 @@ public class HomeWorkFragment extends Fragment {
     Dialog homeData;
     Spinner grade;
     Spinner subject;
-    RadioButton isTest;
+    CheckBox isTest;
     EditText work;
     Button add;
 
